aa60275de643a49bfe22ea9008e38baaf74af3fb
fadhel <fadhel.gaaloul@gmail.com>
2023-07-31 03:37:44 +0100

    create project

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
     images: {
         domains: ['lh3.googleusercontent.com']
+    },
+
+    experimental: {
+        serverComponentsExternalPackages: ['cloudianry', 'garphql-request']
     }
 }
 
