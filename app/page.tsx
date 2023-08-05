4ae5e60f9b7f9e57d76888be7a403dcb9f00e1d8
fadhel <fadhel.gaaloul@gmail.com>
2023-08-05 15:28:36 +0100

    add Categories

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import { ProjectInterface } from "@/common.types"
+import Categories from "@/components/Categories";
 import ProjectCard from "@/components/ProjectCard";
 import { fetchAllProjects } from "@/lib/actions"
 
@@ -33,7 +34,7 @@ const Home = async () => {
 
   return (
     <section className="flex-start flex-col paddings mb-16">
-      <h1>Categories</h1>
+      <Categories></Categories>
       
       <section className="projects-grid">
         {projectToDisplay.map(({ node }: { node: ProjectInterface }) => (
