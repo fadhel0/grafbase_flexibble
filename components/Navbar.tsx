9e66a4dc4caa4e2a54100bc0c0faf8ca916e4d44
fadhel <fadhel.gaaloul@gmail.com>
2023-08-06 18:04:19 +0100

    fix Navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,51 +1,51 @@
+import Image from "next/image";
+import Link from "next/link";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { NavLinks } from '@/constants';
-import  AuthProviders  from './AuthProviders';
-import { getCurrentUser } from '@/lib/session';
-import { signOut } from 'next-auth/react';
-import ProfileMenu from './ProfileMenu';
+import { NavLinks } from "@/constants";
+import { getCurrentUser } from "@/lib/session";
 
-export const Navbar = async () => {
-    const session = await getCurrentUser();
-    
+import AuthProviders from "./AuthProviders";
+import Button from "./Button";
+import ProfileMenu from "./ProfileMenu";
+
+const Navbar = async () => {
+  const session = await getCurrentUser()
+
   return (
     <nav className='flexBetween navbar'>
-        <div className='flex-1 flexStart gap-10'>
-            <Link href="/">
-                <Image
-                    src="/logo.svg"
-                    width={115}
-                    height={43}
-                    alt='Flexible'
-                />
+      <div className='flex-1 flexStart gap-10'>
+        <Link href='/'>
+          <Image
+            src='/logo.svg'
+            width={116}
+            height={43}
+            alt='logo'
+          />
+        </Link>
+        <ul className='xl:flex hidden text-small gap-7'>
+          {NavLinks.map((link) => (
+            <Link href={link.href} key={link.text}>
+              {link.text}
             </Link>
-            <ul className='xl:flex hidden text-small gap-7'>
-                {NavLinks.map((link) => (
-                    <Link href={link.href} key={link.key}>
-                        {link.text}
-                    </Link>
-                ))}
-            </ul>
-        </div>
+          ))}
+        </ul>
+      </div>
 
-        <div className='flexCenter gap-4'>
-            {session?.user ?(
-                <>
-                    <ProfileMenu session= {session}/>
+      <div className='flexCenter gap-4'>
+        {session?.user ? (
+          <>
+            <ProfileMenu session={session} />
 
-                    <Link href="/create-project">
-                        Share Work
-                    </Link>
-
-                    
-                </>
-            ) : (
-              <AuthProviders />  
-            )}
-        </div>
+            <Link href="/create-project">
+              <Button title='Share work' />
+            </Link>
+          </>
+        ) : (
+          <AuthProviders />
+        )}
+      </div>
     </nav>
-    
-  )
-}
+  );
+};
+
+export default Navbar;
