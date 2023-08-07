85f9eccae413ca04ea88bf76cfc87ef78a7bcfd0
fadhel <fadhel.gaaloul@gmail.com>
2023-08-07 01:39:36 +0100

    fix issues

--- lib/actions.ts
+++ lib/actions.ts
@@ -8,7 +8,7 @@ const isProduction = process.env.NODE_ENV === 'production';
 const apiUrl = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || '' : 'http://127.0.0.1:4000/graphql';
 const apiKey = isProduction ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || '' : 'letmein';
 
-const serverUrl = isProduction ? process.env.NEXTA_PUBLIC_SERVER_URL : 'http://localhost:3000';
+const serverUrl = isProduction ? process.env.NEXT_PUBLIC_SERVER_URL : 'http://localhost:3000';
 const client = new GraphQLClient(apiUrl)
 
 export const fetchToken = async () => {
